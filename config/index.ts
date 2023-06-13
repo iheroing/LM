1ab8761bfa212d47bb068701cb4e5030ec939e44
iheroing <106726362+iheroing@users.noreply.github.com>
2023-06-13 19:41:44 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,8 +3,8 @@ export const APP_ID = '89704196-ce95-4e03-bdae-9874a6e57535'
 export const API_KEY = 'app-2tlAjLw8NQzA6a24LzzuFgob'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
+  title: '医学试题答疑助手',
+  description: '帮你解决各种医学试题',
   copyright: '',
   privacy_policy: '',
   default_language: 'zh-Hans',
@@ -17,4 +17,4 @@ export const API_PREFIX = '/api'
 
 export const LOCALE_COOKIE_NAME = 'locale'
 
-export const DEFAULT_VALUE_MAX_LEN = 48
+export const DEFAULT_VALUE_MAX_LEN = 1000
